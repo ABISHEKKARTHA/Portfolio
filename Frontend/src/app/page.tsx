b3117e00da4a91c4152015515245cf8bd0ed4894
ABISHEKKARTHA <67997045+ABISHEKKARTHA@users.noreply.github.com>
2024-06-15 22:17:01 +0530

    Update page.tsx
    removed '

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             <h2>DevOps & SRE Specialist :</h2>
             <h3>Building resilient systems</h3>
             <p>With a strong foundation in continuous integration/continuous deployment (CI/CD) pipelines, cloud infrastructure, and monitoring tools, I thrive in fast-paced environments where precision and reliability are paramount. My proactive approach to problem-solving and my commitment to best practices in DevOps enable me to deliver solutions that not only meet but exceed organizational goals.
-            Let's work together to build resilient systems that stand the test of time.</p>
+            Let us work together to build resilient systems that stand the test of time.</p>
 
             <Button label="Learn more" link="/about"/>
 
